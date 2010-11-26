56f69e1ea21dbccbf6cfea949ec2267acbae372c
Tim Sutton <tim@linfiniti.com>
2010-11-26 08:23:44 +0200

    Updates to dump and edit script so lout compiles

--- course-notes/mapserver/edit.sh
+++ course-notes/mapserver/edit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-vim docs/mapserver.t2t
-txt2tags -t html -o mapserver.html docs/mapserver.t2t
-txt2tags -t lout -o mapserver.lout docs/mapserver.t2t
+vim doc/mapserver.t2t
+txt2tags -t html -o mapserver.html doc/mapserver.t2t
+txt2tags -t lout -o mapserver.lout doc/mapserver.t2t
 
 # lout does direct pdf but its experimental and no images get rendered
 # so we do to eps then convert that to pdf
 # lout -o mapserver.pdf -PDF mapserver.lout
 lout -o mapserver.eps -EPS mapserver.lout
 ps2pdf mapserver.eps
-firefox-3.5 mapserver.html
+firefox mapserver.html
