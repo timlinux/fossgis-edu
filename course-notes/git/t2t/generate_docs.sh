cf69e176f7626b2473caba754d9ae1b191577a08
Tim Sutton <tim@linfiniti.com>
2011-05-10 23:06:08 +0200

    Final update to the git tut for now

--- course-notes/git/t2t/generate_docs.sh
+++ course-notes/git/t2t/generate_docs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 txt2tags -t tex -o LinfinitiGitTutorial.tex index.t2t
 pdflatex LinfinitiGitTutorial.tex
+scp LinfinitiGitTutorial.pdf linfiniti.com:/home/timlinux/linfiniti.com/downloads/
