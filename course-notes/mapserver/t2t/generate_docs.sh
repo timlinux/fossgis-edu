aa3bf7c24d075149a5cb9ae3322cdc86f1f93378
Tim Sutton <tim@linfiniti.com>
2011-05-10 23:44:26 +0200

    Refactor and cleanup for mapserver and use scriptsize for verbatim

---
+++ course-notes/mapserver/t2t/generate_docs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+txt2tags -t tex -o LinfinitiMapserverTutorial.tex mapserver.t2t
+pdflatex LinfinitiMapserverTutorial.tex
+#scp LinfinitiMapserverTutorial.pdf linfiniti.com:/home/timlinux/linfiniti.com/downloads/
