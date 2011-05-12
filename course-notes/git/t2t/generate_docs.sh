45df00c91fdf05facfa1808a3d20eb0b0d9eac07
Tim Sutton <tim@linfiniti.com>
2011-05-12 08:52:50 +0200

    Copy generated pdf into course cd

--- course-notes/git/t2t/generate_docs.sh
+++ course-notes/git/t2t/generate_docs.sh
@@ -2,3 +2,4 @@
 txt2tags -t tex -o LinfinitiGitTutorial.tex index.t2t
 pdflatex LinfinitiGitTutorial.tex
 scp LinfinitiGitTutorial.pdf linfiniti.com:/home/timlinux/linfiniti.com/downloads/
+cp LinfinitiGitTutorial.pdf /home/timlinux/Documents/CourseCDs/QGISInThreeDays/docs/Git
